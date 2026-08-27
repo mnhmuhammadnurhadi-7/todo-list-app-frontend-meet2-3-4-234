1922a3bd167a68c3cc8b0175da9c1ed0e6edf6bc
Muhammad Nur Hadi <mnhmuhammadnurhadi@gmail.com>
2026-08-27 14:21:32 +0700

    frontend

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <div className="text-xl font-semibold text-gray-700">
+        sedang memuat data.......
+      </div>
+    </div>
+  );
+}
